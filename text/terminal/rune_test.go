13864b728a8dfccea0e6d9b9b8d0417ba12cb740
bandesz <bandesz@gmail.com>
2017-06-06 09:48:04 +0100

    Clean up rune test

--- text/terminal/rune_test.go
+++ text/terminal/rune_test.go
@@ -10,23 +10,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func assertCursor(t *testing.T, pos int, line int, col int, r *text.Reader) {
-	assert.Equal(t, text.NewPosition(pos, line, col), r.Cursor())
-}
-
 func TestRuneShouldMatchCharacter(t *testing.T) {
 	r := text.NewReader([]byte("a"), true)
 	_, res := terminal.Rune('a', "A")(parser.EmptyLeftRecCtx(), r)
 	expectedNode := ast.NewTerminalNode("A", text.NewPosition(0, 1, 1), 'a')
 	assert.Equal(t, parser.NewResult(expectedNode, r).AsSet(), res)
-	assertCursor(t, 1, 1, 2, r)
+	assert.Equal(t, text.NewPosition(1, 1, 2), r.Cursor())
 }
 
 func TestRuneShouldNotUseSpecialChars(t *testing.T) {
 	r := text.NewReader([]byte("a"), true)
 	_, res := terminal.Rune('.', ".")(parser.EmptyLeftRecCtx(), r)
 	assert.Nil(t, res)
-	assertCursor(t, 0, 1, 1, r)
+	assert.Equal(t, text.NewPosition(0, 1, 1), r.Cursor())
 }
 
 func TestRuneShouldMatchUnicodeCharacter(t *testing.T) {
@@ -34,12 +30,12 @@ func TestRuneShouldMatchUnicodeCharacter(t *testing.T) {
 	_, res := terminal.Rune('🍕', "PIZZA")(parser.EmptyLeftRecCtx(), r)
 	expectedNode := ast.NewTerminalNode("PIZZA", text.NewPosition(0, 1, 1), '🍕')
 	assert.Equal(t, parser.NewResult(expectedNode, r).AsSet(), res)
-	assertCursor(t, 4, 1, 2, r)
+	assert.Equal(t, text.NewPosition(4, 1, 2), r.Cursor())
 }
 
 func TestRuneShouldNotMatchCharacter(t *testing.T) {
 	r := text.NewReader([]byte("a"), true)
 	_, res := terminal.Rune('b', "B")(parser.EmptyLeftRecCtx(), r)
 	assert.Nil(t, res)
-	assertCursor(t, 0, 1, 1, r)
+	assert.Equal(t, text.NewPosition(0, 1, 1), r.Cursor())
 }
