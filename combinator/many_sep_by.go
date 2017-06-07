a75790bc786dd704c35ef8c5796d25c0a21efef7
bandesz <bandesz@gmail.com>
2017-06-07 18:47:20 +0100

    ManySepBy intermediary parsers don't need an interpreter

--- combinator/many_sep_by.go
+++ combinator/many_sep_by.go
@@ -8,15 +8,15 @@ import (
 
 // ManySepBy matches the given value parser zero or more times separated by the separator parser
 func ManySepBy(token string, h *parser.History, valueP parser.Parser, sepP parser.Parser, interpreter ast.Interpreter) parser.Func {
-	sepValue := Memoize(token+"_MSB", h, And(builder.All("SEP_VALUE", interpreter), sepP, valueP))
-	sepValueMany := Memoize(token+"_MSB*", h, Many(builder.Flatten(token, interpreter), sepValue, 0, -1))
+	sepValue := Memoize(token+"_MSB", h, And(builder.All("SEP_VALUE", nil), sepP, valueP))
+	sepValueMany := Memoize(token+"_MSB*", h, Many(builder.Flatten(token, nil), sepValue, 0, -1))
 	return Try(mergeChildren(token, interpreter), 0, valueP, sepValueMany)
 }
 
 // ManySepBy1 matches the given value parser one or more times separated by the separator parser
 func ManySepBy1(token string, h *parser.History, valueP parser.Parser, sepP parser.Parser, interpreter ast.Interpreter) parser.Func {
-	sepValue := Memoize(token+"_MSB", h, And(builder.All("SEP_VALUE", interpreter), sepP, valueP))
-	sepValueMany := Memoize(token+"_MSB*", h, Many(builder.Flatten(token, interpreter), sepValue, 0, -1))
+	sepValue := Memoize(token+"_MSB", h, And(builder.All("SEP_VALUE", nil), sepP, valueP))
+	sepValueMany := Memoize(token+"_MSB*", h, Many(builder.Flatten(token, nil), sepValue, 0, -1))
 	return Try(mergeChildren(token, interpreter), 1, valueP, sepValueMany)
 }
 
