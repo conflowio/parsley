fed5b66817fedf9d69c856f1bdf77b4993268bbc
bandesz <bandesz@gmail.com>
2017-06-19 15:22:30 +0100

    Add test for parser.NewError

---
+++ parser/error_test.go
@@ -0,0 +1,16 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/opsidian/parsley/parser"
+	"github.com/opsidian/parsley/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewError(t *testing.T) {
+	err := parser.NewError(test.NewPosition(1), "ERR1")
+	assert.Equal(t, test.NewPosition(1), err.Pos())
+	assert.Equal(t, "ERR1", err.Error())
+	assert.Equal(t, "ERR1 at Pos{1}", err.String())
+}
