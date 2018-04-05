0573dded3b6789f27d38e53a3eb79b8817d0b8d2
bandesz <bandesz@gmail.com>
2018-04-05 14:35:13 +0100

    Add missing ginkgo suite files

---
+++ ast/interpreter/interpreter_suite_test.go
@@ -0,0 +1,13 @@
+package interpreter_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestInterpreter(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Interpreter Suite")
+}
