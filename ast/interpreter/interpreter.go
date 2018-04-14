b30735aeef628e5b049dfc55793c1f59dd31e48c
bandesz <bandesz@gmail.com>
2018-04-14 15:06:47 +0100

    Add tests for the interpreters, add generic array and object interpreter

--- ast/interpreter/interpreter.go
+++ ast/interpreter/interpreter.go
@@ -16,7 +16,7 @@ import (
 // Select returns with an interpreter function which returns the value of the selected node
 func Select(i int) ast.InterpreterFunc {
 	return func(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
-		if i >= len(nodes) {
+		if i < 0 || i >= len(nodes) {
 			panic(fmt.Sprintf("node index is out of bounds: %d", i))
 		}
 		return nodes[i].Value(ctx)
@@ -28,4 +28,41 @@ func Nil() ast.InterpreterFunc {
 	return func(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
 		return nil, nil
 	}
+}
+
+// Array can be used to create an array from a list of nodes, where values and separators are following
+// each-other
+func Array() ast.InterpreterFunc {
+	return ast.InterpreterFunc(func(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
+		res := make([]interface{}, (len(nodes)+1)/2)
+		for i := 0; i < len(nodes); i += 2 {
+			value, err := nodes[i].Value(ctx)
+			if err != nil {
+				return nil, err
+			}
+			res[i/2] = value
+		}
+		return res, nil
+	})
+}
+
+// Object can be used to create an object from a list of nodes, where key-value nodes and separater nodes
+// follow each-other, and a key-value node consists of a key node, a separator node and a value node
+func Object() ast.InterpreterFunc {
+	return ast.InterpreterFunc(func(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
+		res := make(map[string]interface{}, (len(nodes)+1)/2)
+		for i := 0; i < len(nodes); i += 2 {
+			keyValue := nodes[i].(*ast.NonTerminalNode)
+			key, err := keyValue.Children()[0].Value(ctx)
+			if err != nil {
+				return nil, err
+			}
+			value, err := keyValue.Children()[2].Value(ctx)
+			if err != nil {
+				return nil, err
+			}
+			res[key.(string)] = value
+		}
+		return res, nil
+	})
 }
