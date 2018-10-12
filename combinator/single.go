ee1c0f1bd5cdd566c0744e11a4586a03f116af83
bandesz <bandesz@gmail.com>
2018-10-12 21:06:41 +0100

    Single combinator should return with the original parser's name

--- combinator/single.go
+++ combinator/single.go
@@ -9,7 +9,7 @@ import (
 
 // Single will change the result of p if it returns with a non terminal node
 // with only one child. In this case directly the child will returned.
-func Single(p parsley.Parser) parser.Func {
+func Single(p parsley.Parser) *parser.NamedFunc {
 	return parser.Func(func(ctx *parsley.Context, leftRecCtx data.IntMap, pos parsley.Pos) (parsley.Node, data.IntSet) {
 		res, cp := p.Parse(ctx, leftRecCtx, pos)
 		if node, ok := res.(*ast.NonTerminalNode); ok {
@@ -19,5 +19,5 @@ func Single(p parsley.Parser) parser.Func {
 		}
 
 		return res, cp
-	})
+	}).WithName(p.Name)
 }
