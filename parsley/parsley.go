95f86c319f7eb491e8c4632749e7262bc3b85777
bandesz <bandesz@gmail.com>
2018-10-13 11:56:13 +0100

    Change the parsley.Parse and parsley.Evaluate methods to return errors with a position

--- parsley/parsley.go
+++ parsley/parsley.go
@@ -7,32 +7,32 @@
 package parsley
 
 import (
-	"fmt"
-
 	"github.com/opsidian/parsley/data"
 )
 
 // Parse parses the given input and returns with the root node of the AST. It expects a reader and the root parser.
 // If there are multiple possible parse trees only the first one is returned.
-func Parse(ctx *Context, p Parser) (Node, Error) {
-	ctx.RegisterCall()
-	node, _ := p.Parse(ctx, data.EmptyIntMap, ctx.Reader().Pos(0))
-	if node == nil {
-		if ctx.Error() != nil {
-			return nil, WrapError(ctx.Error(), "failed to parse the input: {{err}}")
-		}
-		return nil, NewError(ctx.Reader().Pos(0), fmt.Errorf("failed to parse the input: was expecting %s", p.Name()))
+func Parse(ctx *Context, p Parser) (Node, error) {
+	node, _, err := p.Parse(ctx, data.EmptyIntMap, ctx.Reader().Pos(0))
+	if err != nil {
+		return nil, ctx.FileSet().ErrorWithPosition(err)
 	}
+
 	return node, nil
 }
 
 // Evaluate parses the given input and evaluates it. It expects a reader, the root parser and the evaluation context.
 // If there are multiple possible parse trees only the first one is used for evaluation.
-func Evaluate(ctx *Context, p Parser, evalCtx interface{}) (interface{}, Error) {
+func Evaluate(ctx *Context, p Parser, evalCtx interface{}) (interface{}, error) {
 	node, parseErr := Parse(ctx, p)
 	if parseErr != nil {
 		return nil, parseErr
 	}
+
 	value, evalErr := node.Value(evalCtx)
-	return value, evalErr
+	if evalErr != nil {
+		return nil, ctx.FileSet().ErrorWithPosition(evalErr)
+	}
+
+	return value, nil
 }
