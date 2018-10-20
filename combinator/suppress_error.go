d414c7dfcac1a7c61f18c2f7b9267046b1da2118
bandesz <bandesz@gmail.com>
2018-10-20 17:22:46 +0100

    Add SuppressError combinator

---
+++ combinator/suppress_error.go
@@ -0,0 +1,15 @@
+package combinator
+
+import (
+	"github.com/opsidian/parsley/data"
+	"github.com/opsidian/parsley/parser"
+	"github.com/opsidian/parsley/parsley"
+)
+
+// SuppressError removes the error from the parser result
+func SuppressError(p parsley.Parser) parser.Func {
+	return parser.Func(func(ctx *parsley.Context, leftRecCtx data.IntMap, pos parsley.Pos) (parsley.Node, data.IntSet, parsley.Error) {
+		res, cp, _ := p.Parse(ctx, leftRecCtx, pos)
+		return res, cp, nil
+	})
+}
