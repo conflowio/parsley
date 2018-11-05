8e7515c1779bbc6f1fd91595e263729ce7806e20
bandesz <bandesz@gmail.com>
2018-11-05 09:05:58 +0000

    Add missing license headers

--- parsley/file.go
+++ parsley/file.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 package parsley
 
 // File is an interface to translate a byte offset in a file to a position object
